cd24055ce0c3ade5c919e3e633f758dfd1853ca8
山下茜子 <akane@aKnmBa.local>
2017-06-13 22:20:30 +0900

    Initial Commit

---
+++ craft/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  craft
+//
+//  Created by 山下茜子 on 2017/06/13.
+//  Copyright © 2017年 Akane Yamashita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
